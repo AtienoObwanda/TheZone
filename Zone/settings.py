fb319428893047a5b282f8629b2eeb3d75c3a546
atienoobwanda <atienoobwanda@gmail.com>
2022-06-17 12:47:31 +0300

    adding login and logout redirect

--- Zone/settings.py
+++ Zone/settings.py
@@ -126,6 +126,7 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -133,5 +134,16 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'home'
+
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
