c68d9887c1c01134efef544de345b2755f7f9389
atienoobwanda <atienoobwanda@gmail.com>
2022-06-17 13:40:32 +0300

    Adding register users form, the view, the login & register templates

--- members/views.py
+++ members/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404, render
+from .forms import *
 
 def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            form.save()
+            # send_welcome_email(username,email)
+            # HttpResponseRedirect('login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
     return render(request, 'members/register.html') 
 
 def profile(request):
+    
     return render(request, 'members/profile.html') 
 
