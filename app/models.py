186f1918706806621859b0bf7c692d864d87f945
atienoobwanda <atienoobwanda@gmail.com>
2022-06-17 10:07:29 +0300

    adding a model class for neighbourhood

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
+from unicodedata import name
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class neighbourhood(models.Model):
+    zoneName = models.CharField(max_length=255, unique=True)
+    zoneLocation = models.CharField(max_length=255, unique=True)
+    zoneOccupants = models.IntegerField(default=0)
+    zoneAdmin = models.ForeignKey(User, on_delete=models.CASCADE)
+    zoneCreationDate =  models.DateTimeField(default=timezone.now)
