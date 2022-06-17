0ceb65223052a03b67c9f1c6a91592f59d78eb4b
atienoobwanda <atienoobwanda@gmail.com>
2022-06-17 09:53:44 +0300

    Adding the initial configurations and able to run the app

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
+from django import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path(''),
+    path('', views.home, name = 'home'),
 
 ]
--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('account/'),
+    path('account/', views.register, name='register'),
 
 ]
