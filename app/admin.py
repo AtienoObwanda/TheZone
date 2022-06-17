d3cccd18961580cf31ac17ca9347c60d37f42002
atienoobwanda <atienoobwanda@gmail.com>
2022-06-17 12:02:48 +0300

    Registering app models to the admin dashboard

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+
+admin.site.register(business)
+admin.site.register(hood)
+admin.site.register(school)
+admin.site.register(hospital)
+admin.site.register(policeStation)
+
+
+
