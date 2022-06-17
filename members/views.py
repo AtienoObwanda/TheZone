d23364c634a4b3c99c5cb328948a17642d3dbfc8
atienoobwanda <atienoobwanda@gmail.com>
2022-06-17 22:36:42 +0300

    adding views for hood and dashboard

--- members/views.py
+++ members/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import redirect, get_object_or_404, render
 from .forms import *
+from .models import UserProfile
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.views.generic import View
+
 
 def register(request):
     if request.method == 'POST':
@@ -23,3 +27,22 @@ def profile(request):
     
     return render(request, 'members/profile.html') 
 
+class AccountView(LoginRequiredMixin,View):
+    def get(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        user = profile.user
+        zone = profile.zone.all()
+        if zone is None:
+            return render(request, 'members/dashboard.html', {'profile': profile, 'zone': zone, 'profile' : profile})
+
+            
+        else: redirect('profile')
+
+class HoodView(LoginRequiredMixin,View):
+    def get(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        user = profile.user
+        zone = profile.zone.all()
+        if zone:
+            return render('dashboard')
+        else: return render(request, 'members/profile.html', {'user':user})
