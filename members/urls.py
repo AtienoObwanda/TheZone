250c3a5c16b297f7764d458caad72ccc454aae83
atienoobwanda <atienoobwanda@gmail.com>
2022-06-19 06:18:48 +0300

    adding neighborhood images to the project, updating urls

--- members/urls.py
+++ members/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -18,3 +20,5 @@ urlpatterns = [
 
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
